20776911508f14f00fc8090acb783586119cf7d1
Lasse Skaalum <lskaal19@student.aau.dk>
2020-09-15 20:17:59 +0200

    Fixed endless loop on YEP

--- Models/YEP/Yep.js
+++ Models/YEP/Yep.js
@@ -2,8 +2,7 @@ const Model = require(`./../AbstractModels/Model`);
 
 class Yep extends Model {
   handle() {
-    const botId = `752966570094886981`;
-    if (this.msg.author.id !== botId) {
+    if (this.msg.author.bot === false) {
       const yep = global.bot.emojis.cache.find((emoji) => emoji.name === `YEP`);
       this.msg.channel.send(`${yep}`);
     }

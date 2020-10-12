7d3ecf6ede1fbe1f7f036409a71abe7b0d9509f4
Lasse Skaalum <lskaal19@student.aau.dk>
2020-10-12 21:42:20 +0200

    Removed console log from yep

--- Models/MsgContains/YEP/Yep.js
+++ Models/MsgContains/YEP/Yep.js
@@ -4,7 +4,6 @@ class Yep extends Model {
   handle() {
     const yep = global.bot.emojis.cache.find((emoji) => emoji.name === `YEP`);
     this.msg.channel.send(`${yep}`);
-    console.log(yep);
     this.msg.react(yep);
   }
 }

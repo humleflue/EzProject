a37c4bff10a8f7cdad743ca9f9323bc9af149752
Lasse Skaalum <lskaal19@student.aau.dk>
2020-11-16 22:34:50 +0100

    Removed Yep send message (now it only reacts)

--- Models/MsgContains/YEP/Yep.js
+++ Models/MsgContains/YEP/Yep.js
@@ -3,7 +3,7 @@ const Model = require(`../../AbstractModels/Model`);
 class Yep extends Model {
   handle() {
     const yep = global.bot.emojis.cache.find((emoji) => emoji.name === `YEP`);
-    this.msg.channel.send(`${yep}`);
+    // this.msg.channel.send(`${yep}`);
     this.msg.react(yep);
   }
 }
